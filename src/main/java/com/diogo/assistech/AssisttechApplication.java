b41e36cc51f0a087bc08aa5227a26771bcd9c8b8
Diog007 <diogo.mz123@gmail.com>
2024-01-10 20:32:38 -0300

    Projeto iniciado

---
+++ src/main/java/com/diogo/assistech/AssisttechApplication.java
@@ -0,0 +1,13 @@
+package com.diogo.assistech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssisttechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssisttechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diogo/assistech/AssisttechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogo.assistech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssisttechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
