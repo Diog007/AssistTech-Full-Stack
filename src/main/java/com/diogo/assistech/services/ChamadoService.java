21a93ece3055b5e4db9db5e3e8f5f169a7b62db1
Diog007 <diogo.mz123@gmail.com>
2024-01-14 15:27:37 -0300

    FindById para Chamado

---
+++ src/main/java/com/diogo/assistech/services/ChamadoService.java
@@ -0,0 +1,24 @@
+package com.diogo.assistech.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diogo.assistech.domain.Chamado;
+import com.diogo.assistech.repositories.ChamadoRepository;
+import com.diogo.assistech.services.exception.ObjectnotFoundException;
+
+@Service
+public class ChamadoService {
+
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID:" + id));
+	}
+	
+	
+}
