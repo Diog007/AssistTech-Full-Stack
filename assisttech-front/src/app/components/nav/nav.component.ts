9676f38a8a2744a3e7a0aeabfe648b070b4c4725
Diog007 <diogo.mz123@gmail.com>
2024-05-20 20:35:10 -0300

    Route to HOME component

--- assisttech-front/src/app/components/nav/nav.component.ts
+++ assisttech-front/src/app/components/nav/nav.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [MatSidenavModule, MatListModule, MatIconModule],
+  imports: [MatSidenavModule, MatListModule, MatIconModule,RouterOutlet],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+      this.router.navigate(['home'])
+  }
 
 }
