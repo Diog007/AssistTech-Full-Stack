a13648037b838066e1f9d2fae03cd7298a5eae22
Diog007 <diogo.mz123@gmail.com>
2024-05-20 19:23:25 -0300

    HTML and CSS for NAV component

--- assisttech-front/src/app/components/nav/nav.component.ts
+++ assisttech-front/src/app/components/nav/nav.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatListModule, MatIconModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
