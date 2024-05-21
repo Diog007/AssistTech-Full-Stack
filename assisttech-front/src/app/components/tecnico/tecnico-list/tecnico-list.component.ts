a653e4129df74d96168568174114ce2dae8d185e
Diog007 <diogo.mz123@gmail.com>
2024-05-20 23:54:47 -0300

    HTML and CSS for TECNICO-LIST

--- assisttech-front/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ assisttech-front/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Tecnico } from '../../../models/tecnicos';
 
 @Component({
   selector: 'app-tecnico-list',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatPaginatorModule],
   templateUrl: './tecnico-list.component.html',
   styleUrl: './tecnico-list.component.css'
 })
 export class TecnicoListComponent {
 
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Diogo Nascimento',
+      cpf: '123.456.789-10',
+      email: 'diogo.mz@gmail.com',
+      senha: '1234',
+      perfis: ['0'],
+      dataCriacao: '20/05/2024'
+    }
+  ]
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
+
+
