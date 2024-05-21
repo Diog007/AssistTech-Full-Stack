aae50b608f8b8ef3f2f8bda1b3c448ae95c0ba79
Diog007 <diogo.mz123@gmail.com>
2024-05-21 11:29:31 -0300

    Validation of login forms

--- assisttech-front/src/app/components/login/login.component.ts
+++ assisttech-front/src/app/components/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import { Credenciais } from '../../models/credenciais';
 
 
 @Component({
@@ -13,5 +14,21 @@ import {MatButtonModule} from '@angular/material/button';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
+
+  email = new FormControl(null, Validators.email)
+  senha = new FormControl(null, Validators.minLength(3))
+
+  validaCampos(): boolean {
+    if(this.email.valid && this.senha.valid) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 }
