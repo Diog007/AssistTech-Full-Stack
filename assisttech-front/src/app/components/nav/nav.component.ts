62ddd27d7d7b1e9713398ecd33fe04f7f95bc414
Diog007 <diogo.mz123@gmail.com>
2024-05-20 23:30:44 -0300

    Route to TECNICO-LIST

--- assisttech-front/src/app/components/nav/nav.component.ts
+++ assisttech-front/src/app/components/nav/nav.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [MatSidenavModule, MatListModule, MatIconModule,RouterOutlet, HeaderComponent],
+  imports: [MatSidenavModule, MatListModule, MatIconModule,RouterOutlet, HeaderComponent, RouterLink],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
