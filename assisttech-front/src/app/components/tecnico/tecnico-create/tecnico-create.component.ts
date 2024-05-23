97ee51540e09d0b33e9229dd67cdfa71d13305bb
Diog007 <diogo.mz123@gmail.com>
2024-05-23 11:05:15 -0300

    Validation of forms in the TECNICO-CREATE component

--- assisttech-front/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ assisttech-front/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatPseudoCheckboxModule } from '@angular/material/core';
 import {MatRadioModule} from '@angular/material/radio';
@@ -8,6 +8,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 
 
@@ -17,10 +19,25 @@ import {MatButtonModule} from '@angular/material/button';
   imports: [
     MatCardModule, MatPseudoCheckboxModule, FormsModule, MatRadioModule,
      MatCheckboxModule, MatFormFieldModule, MatFormFieldModule, MatButtonModule, 
-     MatInputModule, MatIconModule ],
+     MatInputModule, MatIconModule, RouterLink, ReactiveFormsModule, NgxMaskDirective ],
   templateUrl: './tecnico-create.component.html',
-  styleUrl: './tecnico-create.component.css'
+  styleUrl: './tecnico-create.component.css',
+  providers: [provideNgxMask()]
 })
-export class TecnicoCreateComponent {
+export class TecnicoCreateComponent implements OnInit {
+
+  nome: FormControl = new FormControl(null, Validators.minLength(3))
+  cpf: FormControl = new FormControl(null, Validators.required)
+  email: FormControl = new FormControl(null, Validators.email)
+  senha: FormControl = new FormControl(null, Validators.minLength(3))
+
+  ngOnInit(): void {
+  }
+
+  validaCampos(): boolean {
+    return this.nome.valid && this.cpf.valid 
+    && this.email.valid && this.senha.valid
+  }
+
 
 }
