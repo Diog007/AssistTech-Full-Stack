f2784dcdad0b3f659679a01c7d4059754bd481fd
Diog007 <diogo.mz123@gmail.com>
2024-05-23 12:54:11 -0300

    Technician action buttons

--- assisttech-front/src/app/components/nav/nav.component.ts
+++ assisttech-front/src/app/components/nav/nav.component.ts
@@ -22,7 +22,7 @@ export class NavComponent implements OnInit {
     private toast: ToastrService) { }
 
   ngOnInit(): void {
-      this.router.navigate(['tecnico/create'])
+      this.router.navigate(['tecnicos'])
   }
       
   logout() {
