bb160f6d4caca5f12e619f8dde7e5b1d6c9408c5
Diog007 <diogo.mz123@gmail.com>
2024-05-23 10:35:49 -0300

    HTML and CSS for TECNICO-CREATE component

--- assisttech-front/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ assisttech-front/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatPseudoCheckboxModule } from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-tecnico-create',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule, MatPseudoCheckboxModule, FormsModule, MatRadioModule,
+     MatCheckboxModule, MatFormFieldModule, MatFormFieldModule, MatButtonModule, 
+     MatInputModule, MatIconModule ],
   templateUrl: './tecnico-create.component.html',
   styleUrl: './tecnico-create.component.css'
 })
