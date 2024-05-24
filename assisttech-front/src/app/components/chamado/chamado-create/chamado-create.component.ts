8ad4f1426bfcd223ec17e396381e7e477435adf7
Diog007 <diogo.mz123@gmail.com>
2024-05-24 16:14:18 -0300

    Validation of forms for CALLED-CREATE

--- assisttech-front/src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ assisttech-front/src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
@@ -8,10 +8,28 @@ import { MatSelectModule } from '@angular/material/select';
 @Component({
   selector: 'app-chamado-create',
   standalone: true,
-  imports: [ FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule],
+  imports: [ FormsModule, MatFormFieldModule,
+     MatInputModule, MatSelectModule,
+      MatButtonModule, ReactiveFormsModule],
   templateUrl: './chamado-create.component.html',
   styleUrl: './chamado-create.component.css'
 })
-export class ChamadoCreateComponent {
+export class ChamadoCreateComponent implements OnInit{
+
+  prioridade: FormControl = new FormControl(null, [Validators.required])
+  status:   FormControl = new FormControl(null, [Validators.required])
+  titulo:   FormControl = new FormControl(null, [Validators.required])
+  descricao:  FormControl = new FormControl(null, [Validators.required])
+  tecnico:    FormControl = new FormControl(null, [Validators.required])
+  cliente:    FormControl = new FormControl(null, [Validators.required])
+
+  ngOnInit(): void {
+  }
+
+  validaCampos(): boolean {
+    return this.prioridade.valid && this.status.valid &&
+      this.titulo.valid && this.descricao.valid &&
+      this.tecnico.valid && this.cliente.valid 
+  }
 
 }
