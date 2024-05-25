766c45cb138f63a051aa3be15db1589f9d748fb7
Diog007 <diogo.mz123@gmail.com>
2024-05-25 03:18:05 -0300

    Componente CHAMADO-READ finalizado

--- assisttech-front/src/app/components/chamado/chamado-read/chamado-read.component.ts
+++ assisttech-front/src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -1,12 +1,74 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Chamado } from '../../../models/chamado';
+import { ChamadoService } from '../../../services/chamado.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-chamado-read',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule,
+    MatInputModule, MatSelectModule,
+     MatButtonModule, ReactiveFormsModule, 
+     CommonModule, RouterLink],
   templateUrl: './chamado-read.component.html',
   styleUrl: './chamado-read.component.css'
 })
-export class ChamadoReadComponent {
+export class ChamadoReadComponent implements OnInit{
 
+  chamado: Chamado = {
+    prioridade:  '',
+    status:      '',
+    titulo:      '',
+    observacoes:   '',
+    tecnico:     '',
+    cliente:     '',
+    nomeCliente: '',
+    nomeTecnico: '',
+  }
+
+  constructor(
+    private chamadoService: ChamadoService,
+    private toastService:   ToastrService,
+    private route: ActivatedRoute
+  ) { }
+  
+  ngOnInit(): void {
+    this.chamado.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void {
+    this.chamadoService.findById(this.chamado.id).subscribe(resposta => {
+      this.chamado = resposta;
+    }, ex => {
+      this.toastService.error(ex.error.error);
+    })
+  }
+
+  retornaStatus(status: any): string {
+    if (status == '0') {
+      return 'ABERTO'
+    } else if (status == '1') {
+      return 'EM ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if (prioridade == '0') {
+      return 'BAIXA'
+    } else if (prioridade == '1') {
+      return 'MÉDIA'
+    } else {
+      return 'ALTA'
+    }
+  }
 }
