49e2df37f034afb03f456041ee424b1771c68210
Diog007 <diogo.mz123@gmail.com>
2024-05-24 22:02:57 -0300

    HTML and CSS for CALLED-UPDATE component

--- assisttech-front/src/app/components/chamado/chamado-update/chamado-update.component.ts
+++ assisttech-front/src/app/components/chamado/chamado-update/chamado-update.component.ts
@@ -1,12 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { Cliente } from '../../../models/cliente';
+import { Tecnico } from '../../../models/tecnicos';
+import { ClienteService } from '../../../services/cliente.service';
+import { TecnicoService } from '../../../services/tecnico.service';
+import { ChamadoService } from '../../../services/chamado.service';
+import { Chamado } from '../../../models/chamado';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { Router, RouterLink } from '@angular/router';
+import { NgxMaskDirective } from 'ngx-mask';
+import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-chamado-update',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule,
+    MatInputModule, MatSelectModule,
+     MatButtonModule, ReactiveFormsModule, 
+     CommonModule, RouterLink],
   templateUrl: './chamado-update.component.html',
   styleUrl: './chamado-update.component.css'
 })
-export class ChamadoUpdateComponent {
+export class ChamadoUpdateComponent implements OnInit{
+
+  chamado: Chamado = {
+    prioridade:  '',
+    status:      '',
+    titulo:      '',
+    observacoes:   '',
+    tecnico:     '',
+    cliente:     '',
+    nomeCliente: '',
+    nomeTecnico: '',
+  }
+
+  clientes: Cliente[] = []
+  tecnicos: Tecnico[] = []
+
+
+  
+  prioridade: FormControl = new FormControl(null, [Validators.required])
+  status:     FormControl = new FormControl(null, [Validators.required])
+  titulo:     FormControl = new FormControl(null, [Validators.required])
+  observacoes:  FormControl = new FormControl(null, [Validators.required])
+  tecnico:    FormControl = new FormControl(null, [Validators.required])
+  cliente:    FormControl = new FormControl(null, [Validators.required])
+
+  constructor(
+    private chamadoService: ChamadoService,
+    private clienteService: ClienteService,
+    private tecnicoService: TecnicoService,
+    private toastService:   ToastrService,
+    private router: Router
+  ) { }
+  
+  ngOnInit(): void {
+    this.findAllClientes();
+    this.findAllTecnicos();
+  }
+
+  create(): void {
+    this.chamadoService.create(this.chamado).subscribe (resposta => {
+      this.toastService.success('Chamado realizado com sucesso!', "Novo chamado");
+      this.router.navigate(['chamados']);     
+    }, ex => {
+      console.log(ex);
+      this.toastService.error(ex.error.error);
+    })
+  }
+
+  findAllClientes(): void {
+    this.clienteService.findAll().subscribe(respostaCliente => {
+      this.clientes = respostaCliente;
+    })
+  }
+
+  findAllTecnicos(): void {
+    this.tecnicoService.findAll().subscribe(respostaTecnico => {
+      this.tecnicos = respostaTecnico;
+    })
+  }
+
+  validaCampos(): boolean {
+    return this.prioridade.valid && this.status.valid &&
+      this.titulo.valid && this.observacoes.valid &&
+      this.tecnico.valid && this.cliente.valid 
+  }
 
 }
+
